7f43534806009cb6c2d7bdc506b3412720dd8b65
shortnd <ococncol@gmail.com>
2018-09-03 22:10:37 -0400

    connected frontend to backend

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    This file will list all the posts
+
+    <div v-for="(post, index) in posts" :key="index">
+      <p>
+        <span><b>{{ post.title }}</b></span>
+        <span>{{ post.description }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostsServices from '@/services/PostsServices'
+export default {
+  name: 'posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsServices.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
