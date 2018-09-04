6b18b511a99fe8904af99483a3908a861cf9cf98
shortnd <ococncol@gmail.com>
2018-09-03 23:01:31 -0400

    can now post to create posts

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+var mongoose = require('mongoose')
+var Post = require('../models/post')
+
+mongoose.connect('mongodb://localhost:27017/posts')
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function(callback){
+  console.log('Connection Succeeded');
+})
 
 const app = express()
 app.use(morgan('combined'))
@@ -9,12 +18,32 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: 'Hello World',
-      description: 'Hi there how are you?'
-    }]
-  )
+  Post.find({}, 'title description', function(error, posts) {
+    if (error) {console.error(error);}
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1})
+})
+
+app.post('/posts', (req, res) => {
+  var db = req.db
+  var title = req.body.title
+  var description = req.body.description
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error);
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully'
+    })
+  })
 })
 
 app.listen(process.env.PORT || 8081)
